51219ae168a72aaa541604ac54f9ebd7203d5d69
greenojo <grizmog@gmail.com>
2015-10-14 12:58:01 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,36 +1,4 @@
-<?php 
-/*
-	Dude What's My IP? Version 2.0
-	@ http://perishablepress.com/roll-your-own-whats-my-ip/
-*/
-
-// variables
-$xfwd     = mm_strip($_SERVER["HTTP_X_FORWARDED_FOR"]); 
-$address  = mm_strip($_SERVER["REMOTE_ADDR"]); 
-$port     = mm_strip($_SERVER["REMOTE_PORT"]); 
-$method   = mm_strip($_SERVER["REQUEST_METHOD"]); 
-$protocol = mm_strip($_SERVER["SERVER_PROTOCOL"]); 
-$agent    = mm_strip($_SERVER["HTTP_USER_AGENT"]); 
-
-if ($xfwd !== '') {
-	$IP = $xfwd;
-	$proxy = $address;
-	$host = @gethostbyaddr($xfwd);
-} else {
-	$IP = $address;
-	$host = @gethostbyaddr($address);
-}
-
-// sanitizes
-function mm_strip($string) {
-	$string = trim($string); 
-	$string = strip_tags($string);
-	$string = htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
-	$string = str_replace("\n", "", $string);
-	$string = trim($string); 
-	return $string;
-}
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title>What's my IP dude!</title>
 	<style type="text/css">
